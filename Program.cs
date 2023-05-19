5ee4ed8823b24349a303e0fb921b2668bc767e97
t4w4ntm <122960758+t4w4ntm@users.noreply.github.com>
2023-05-19 20:40:59 +0700

    road

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("ช่วงถนน : ");
+        int count = int.Parse(Console.ReadLine());
+
+        Console.Write("ขอบเขต : ");
+        int road = int.Parse(Console.ReadLine());
+
+        int Newroad = road+(road+1);
+
+        int[] numbers = new int[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write("จำนวนลูกค้าเขตที่ " + (i + 1) + ": ");
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        int maxSum = 0;
+        int maxSumIndex = 0;
+
+        for (int i = 0; i < count - Newroad + 1; i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < Newroad; j++)
+            {
+                sum += numbers[i + j];
+            }
+            if (sum > maxSum)
+            {
+                maxSum = sum;
+                maxSumIndex = i;
+            }
+        }
+
+        Console.Write("จำนวนลูกค้ารวมสูงสุด : ");
+        
+        Console.WriteLine(maxSum);
+    }
+}
